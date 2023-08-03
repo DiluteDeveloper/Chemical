861ba6b55d6a2a6b313772570059caa52977b613
DiluteDev <dilutepersonal@gmail.com>
2023-08-03 10:53:35 +1000

    changed chunk rendering to iterate thru block side faces

--- README.txt
+++ README.txt
@@ -2,3 +2,6 @@ todo:
 
 Add 0 stride warning to vertex layout class
 remove vertex layout from shaderprogram class
+Combine chunk rendering and chunk generation
+combine 2 for loops for each axis of chunk side face generation into one, they are split because one iterates over negative,
+one iterates over positive.
--- projects/chemical/src/chunk_system.cpp
+++ projects/chemical/src/chunk_system.cpp
@@ -20,6 +20,7 @@ Chunk::Chunk(const glm::dvec3& origin) :
 			int genHeight = perlin.normalizedOctave2D(x * 0.01f, z * 0.01f, 1) * 20;
 			genHeight += perlin2.normalizedOctave2D(x * 0.02f, z * 0.02f, 4) * 30;
 			genHeight += perlin3.normalizedOctave2D(x * 0.004f, z * 0.004f, 1) * 80;
+			genHeight += perlin3.normalizedOctave2D(x * 0.01f, z * 0.01f, 1) * 120;
 
 			visibleBlocks[x][z].emplace_back(genHeight); // generate the top block
 
@@ -50,6 +51,7 @@ Chunk::Chunk(const glm::dvec3& origin) :
 	}
 }
 
+// Its a bit inneficient to do the rendering separately so maybe combine them at some point
 std::shared_ptr<ChunkRender> RenderChunk(Chunk& chunk, const OpenGL::ShaderProgram& p) {
 	std::vector<ChunkVertex> vertices;
 
@@ -57,52 +59,82 @@ std::shared_ptr<ChunkRender> RenderChunk(Chunk& chunk, const OpenGL::ShaderProgr
 	{
 		for (size_t z = 0; z < CHUNK_SIZE_Z; z++)
 		{
-			const int16_t topBlockY = chunk.visibleBlocks[x][z][0];
+			const int16_t height = chunk.visibleBlocks[x][z][0];
 
-			vertices.emplace_back(glm::vec3(x - 0.5f, topBlockY, z - 0.5f), glm::vec3(0, 1, 0));
-			vertices.emplace_back(glm::vec3(x + 0.5f, topBlockY, z - 0.5f), glm::vec3(0, 1, 0));
-			vertices.emplace_back(glm::vec3(x - 0.5f, topBlockY, z + 0.5f), glm::vec3(0, 1, 0));
+			vertices.emplace_back(glm::vec3(x - 0.5f, height, z - 0.5f), glm::vec3(0, 1, 0));
+			vertices.emplace_back(glm::vec3(x + 0.5f, height, z - 0.5f), glm::vec3(0, 1, 0));
+			vertices.emplace_back(glm::vec3(x - 0.5f, height, z + 0.5f), glm::vec3(0, 1, 0));
 
-			vertices.emplace_back(glm::vec3(x - 0.5f, topBlockY, z + 0.5f), glm::vec3(0, 1, 0));
-			vertices.emplace_back(glm::vec3(x + 0.5f, topBlockY, z - 0.5f), glm::vec3(0, 1, 0));
-			vertices.emplace_back(glm::vec3(x + 0.5f, topBlockY, z + 0.5f), glm::vec3(0, 1, 0));
+			vertices.emplace_back(glm::vec3(x - 0.5f, height, z + 0.5f), glm::vec3(0, 1, 0));
+			vertices.emplace_back(glm::vec3(x + 0.5f, height, z - 0.5f), glm::vec3(0, 1, 0));
+			vertices.emplace_back(glm::vec3(x + 0.5f, height, z + 0.5f), glm::vec3(0, 1, 0));
 
 			// X+ face
 
 			if (x < CHUNK_SIZE_X - 1) {
-				int16_t xp_height = chunk.visibleBlocks[x + 1][z][0];
 
+				// height diff is next - current e.g 20 - 18 = 2
+				int16_t heightDiff = chunk.visibleBlocks[x + 1][z][0] - chunk.visibleBlocks[x][z][0] ;
 
-				if (xp_height != topBlockY) {
-					vertices.emplace_back(glm::vec3(x + 0.5f, topBlockY, z - 0.5f), glm::vec3(1, 0, 0));
-					vertices.emplace_back(glm::vec3(x + 0.5f, xp_height, z - 0.5f), glm::vec3(1, 0, 0));
-					vertices.emplace_back(glm::vec3(x + 0.5f, topBlockY, z + 0.5f), glm::vec3(1, 0, 0));
+				if (heightDiff < 0) // if it is equal to 0, the positive branch will not run either
+					for (int16_t i = 0; i > heightDiff; i--)
+					{
+						vertices.emplace_back(glm::vec3(x + 0.5f, height, z - 0.5f), glm::vec3(1, 0, 0));
+						vertices.emplace_back(glm::vec3(x + 0.5f, (height - 1) + i, z - 0.5f), glm::vec3(1, 0, 0));
+						vertices.emplace_back(glm::vec3(x + 0.5f, height, z + 0.5f), glm::vec3(1, 0, 0));
 
-					vertices.emplace_back(glm::vec3(x + 0.5f, topBlockY, z + 0.5f), glm::vec3(1, 0, 0));
-					vertices.emplace_back(glm::vec3(x + 0.5f, xp_height, z - 0.5f), glm::vec3(1, 0, 0));
-					vertices.emplace_back(glm::vec3(x + 0.5f, xp_height, z + 0.5f), glm::vec3(1, 0, 0));
+						vertices.emplace_back(glm::vec3(x + 0.5f, height, z + 0.5f), glm::vec3(1, 0, 0));
+						vertices.emplace_back(glm::vec3(x + 0.5f, (height - 1) + i, z - 0.5f), glm::vec3(1, 0, 0));
+						vertices.emplace_back(glm::vec3(x + 0.5f, (height - 1) + i, z + 0.5f), glm::vec3(1, 0, 0));
+					}
+				else 
+					for (int16_t i = 0; i < heightDiff; i++)
+					{
+						vertices.emplace_back(glm::vec3(x + 0.5f, height, z - 0.5f), glm::vec3(0, 0, 1));
+						vertices.emplace_back(glm::vec3(x + 0.5f, (height + 1) + i, z - 0.5f), glm::vec3(0, 0, 1));
+						vertices.emplace_back(glm::vec3(x + 0.5f, height, z + 0.5f), glm::vec3(0, 0, 1));
 
-				}
+						vertices.emplace_back(glm::vec3(x + 0.5f, height, z + 0.5f), glm::vec3(0, 0, 1));
+						vertices.emplace_back(glm::vec3(x + 0.5f, (height + 1) + i, z - 0.5f), glm::vec3(0, 0, 1));
+						vertices.emplace_back(glm::vec3(x + 0.5f, (height + 1) + i, z + 0.5f), glm::vec3(0, 0, 1));
+					}
 			}
+
+			// The only reason to need 2 for loops each is to account for a negative for loop and a positive for loop. maybe this can be fixed?
 
 			// Z+ face
 
 			if (z < CHUNK_SIZE_Z - 1) {
-				int16_t zp_height = chunk.visibleBlocks[x][z + 1][0];
 
+				// height diff is next - current e.g 20 - 18 = 2
+				int16_t heightDiff = chunk.visibleBlocks[x][z + 1][0] - chunk.visibleBlocks[x][z][0];
 
-				if (zp_height != topBlockY) {
+				if (heightDiff < 0) // if it is equal to 0, the positive branch will not run either
+					for (int16_t i = 0; i > heightDiff; i--)
+					{
+						vertices.emplace_back(glm::vec3(x + 0.5f, height, z + 0.5f), glm::vec3(0, 0, 1));
+						vertices.emplace_back(glm::vec3(x - 0.5f, (height - 1) + i, z + 0.5f), glm::vec3(0, 0, 1));
+						vertices.emplace_back(glm::vec3(x - 0.5f, height, z + 0.5f), glm::vec3(0, 0, 1));
 
-					vertices.emplace_back(glm::vec3(x + 0.5f, topBlockY, z + 0.5f), glm::vec3(0, 0, 1));
-					vertices.emplace_back(glm::vec3(x - 0.5f, zp_height, z + 0.5f), glm::vec3(0, 0, 1));
-					vertices.emplace_back(glm::vec3(x - 0.5f, topBlockY, z + 0.5f), glm::vec3(0, 0, 1));
+						vertices.emplace_back(glm::vec3(x + 0.5f, (height - 1) + i, z + 0.5f), glm::vec3(0, 0, 1));
+						vertices.emplace_back(glm::vec3(x - 0.5f, (height - 1) + i, z + 0.5f), glm::vec3(0, 0, 1));
+						vertices.emplace_back(glm::vec3(x + 0.5f, height, z + 0.5f), glm::vec3(0, 0, 1));
+					}
+				else
+					for (int16_t i = 0; i < heightDiff; i++)
+					{
+						vertices.emplace_back(glm::vec3(x + 0.5f, height, z + 0.5f), glm::vec3(0, 0, 1));
+						vertices.emplace_back(glm::vec3(x - 0.5f, (height + 1) + i, z + 0.5f), glm::vec3(0, 0, 1));
+						vertices.emplace_back(glm::vec3(x - 0.5f, height, z + 0.5f), glm::vec3(0, 0, 1));
 
-					vertices.emplace_back(glm::vec3(x + 0.5f, zp_height, z + 0.5f), glm::vec3(0, 0, 1));
-					vertices.emplace_back(glm::vec3(x - 0.5f, zp_height, z + 0.5f), glm::vec3(0, 0, 1));
-					vertices.emplace_back(glm::vec3(x + 0.5f, topBlockY, z + 0.5f), glm::vec3(0, 0, 1));
-
-				}
+						vertices.emplace_back(glm::vec3(x + 0.5f, (height + 1) + i, z + 0.5f), glm::vec3(0, 0, 1));
+						vertices.emplace_back(glm::vec3(x - 0.5f, (height + 1) + i, z + 0.5f), glm::vec3(0, 0, 1));
+						vertices.emplace_back(glm::vec3(x + 0.5f, height, z + 0.5f), glm::vec3(0, 0, 1));
+					}
 			}
+			
+
+			// Z+ face
 
 		}
 	}
