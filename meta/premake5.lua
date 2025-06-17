43368789cbb0a0c16ed511754fa0231ba51ebc4d
DiluteDeveloper <dilutepersonal@gmail.com>
2025-06-17 12:17:55 +1000

    C rewrite 17-07-2025

--- meta/premake5.lua
+++ meta/premake5.lua
@@ -3,10 +3,9 @@ location("../")
 configurations({ "Debug", "Release" })
 startproject("Chemical")
 architecture("x86_64")
-language("C++")
-cppdialect("C++23")
+language("C")
+cdialect("C99")
 systemversion("latest")
-debugformat("c7")
 toolset("clang")
 
 filter("configurations:Debug")
@@ -24,13 +23,7 @@ MODULES_DIR = "%{wks.location}/modules"
 IMPORTED_MODULES_DIR = MODULES_DIR .. "/imported"
 
 CHEMICAL_DIR = MODULES_DIR .. "/chemical"
-CHEMICAL_INCLUDE_DIR = CHEMICAL_DIR .. "/include"
-CHEMICAL_DEV_INCLUDE_DIR = CHEMICAL_DIR .. "/include/chemical/"
 CHEMICAL_SOURCE_DIR = CHEMICAL_DIR .. "/src"
-
-SANDBOX_DIR = MODULES_DIR .. "/sandbox"
-SANDBOX_INCLUDE_DIR = SANDBOX_DIR .. "/include"
-SANDBOX_SOURCE_DIR = SANDBOX_DIR .. "/src"
 
 GLFW_LIB_DIR = IMPORTED_MODULES_DIR .. "/glfw"
 GLAD_LIB_DIR = IMPORTED_MODULES_DIR .. "/glad"
@@ -40,30 +33,21 @@ GLAD_LIB = "glad"
 project("Chemical")
 kind("ConsoleApp")
 
--- pchheader("pch.h")
--- pchsource("../modules/chemical/src/pch.cpp") -- relative to this script: has to be changed manually
-
 defines("GLFW_INCLUDE_NONE")
 
 location(CHEMICAL_DIR)
 files({
 	CHEMICAL_SOURCE_DIR .. "/**",
-	SANDBOX_SOURCE_DIR .. "/**",
-	SANDBOX_INCLUDE_DIR .. "/**",
-	CHEMICAL_INCLUDE_DIR .. "/**",
 })
-includedirs({ IMPORTED_MODULES_DIR, CHEMICAL_DEV_INCLUDE_DIR, SANDBOX_INCLUDE_DIR })
+includedirs({ IMPORTED_MODULES_DIR, CHEMICAL_SOURCE_DIR })
 
 libdirs({ GLFW_LIB_DIR, GLAD_LIB_DIR })
-links({ GLFW_LIB, GLAD_LIB })
+
+filter("system:windows")
+links({ "OpenGL32", GLFW_LIB, GLAD_LIB })
+
+filter("system:linux")
+links({ "m", GLFW_LIB, GLAD_LIB })
 
 targetdir(BIN_DIR)
 objdir(INT_DIR)
-
-project("Sandbox")
-kind("Utility")
-
-location(SANDBOX_DIR)
-
--- Imported modules inclusion is a quick-fix for glm
-includedirs({ SANDBOX_INCLUDE_DIR, CHEMICAL_INCLUDE_DIR, IMPORTED_MODULES_DIR })
